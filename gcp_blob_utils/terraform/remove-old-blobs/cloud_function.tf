430e43eaf53132e8d31fa89042d5a3e735548c0e
Mikhail Akhmoev <mikhail.akhmoev@sada.com>
2022-01-03 23:55:15 +0000

    added md5 to source code archive to trigger cloud function redeployment

--- gcp_blob_utils/terraform/remove-old-blobs/cloud_function.tf
+++ gcp_blob_utils/terraform/remove-old-blobs/cloud_function.tf
@@ -1,17 +1,20 @@
 data "google_storage_bucket" "staging_bucket" {
-  name     = var.staging_bucket
+  name = var.staging_bucket
 }
 
 data "archive_file" "archive_remove_old_blobs" {
-    type = "zip"
-    source_dir = "${path.module}/../../src"
-    output_path = "${path.module}/../../bin/remove_old_blobs.zip"
+  type        = "zip"
+  source_dir  = "${path.module}/../../src"
+  output_path = "${path.module}/../../bin/remove_old_blobs.zip"
 }
 
 resource "google_storage_bucket_object" "gcs_remove_old_blobs" {
-  name   = "tmp/cloud-functions/remove-old-blobs.zip"
-  bucket = data.google_storage_bucket.staging_bucket.name
-  source = data.archive_file.archive_remove_old_blobs.output_path
+  name                = format("tmp/cloud-functions/remove-old-blobs.zip#%s", data.archive_file.archive_remove_old_blobs.output_md5)
+  bucket              = data.google_storage_bucket.staging_bucket.name
+  source              = data.archive_file.archive_remove_old_blobs.output_path
+  content_disposition = "attachment"
+  content_encoding    = "gzip"
+  content_type        = "application/zip"
 }
 
 resource "google_cloudfunctions_function" "fn_remove_old_blobs" {
@@ -24,12 +27,12 @@ resource "google_cloudfunctions_function" "fn_remove_old_blobs" {
   source_archive_object = google_storage_bucket_object.gcs_remove_old_blobs.name
   trigger_http          = true
   entry_point           = "remove_old_blobs"
-  region = var.cloud_function_region
+  region                = var.cloud_function_region
 
   environment_variables = {
     BLOB_BUCKET = var.blob_bucket
-    BLOB_PATH = var.blob_path
-    TTL_DAYS = var.ttl_days
+    BLOB_PATH   = var.blob_path
+    TTL_DAYS    = var.ttl_days
   }
 }
 
